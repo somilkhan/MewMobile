39394b854e7619af6b99c290a048174a11a3f296
Zen <81418397+somilkhan@users.noreply.github.com>
2026-09-23 02:59:49 +0530

    Remove obsolete dynamic repository event bridge

--- composeApp/src/androidPlaystore/kotlin/com/nuvio/app/features/cloudstream/CloudStreamPlatformRuntime.android.kt
+++ composeApp/src/androidPlaystore/kotlin/com/nuvio/app/features/cloudstream/CloudStreamPlatformRuntime.android.kt
@@ -5,7 +5,6 @@ internal actual object CloudStreamPlatformRuntime {
 
     actual fun initialize(context: Any?) = Unit
     actual suspend fun provider(plugin: CloudStreamPluginItem): CloudStreamProvider? = null
-    actual suspend fun syncDynamicRepositories(plugin: CloudStreamPluginItem) = Unit
     actual fun unload(pluginId: String) = Unit
     actual fun clear() = Unit
 }
