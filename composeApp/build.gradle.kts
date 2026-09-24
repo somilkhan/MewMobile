2348bcd9ee7d4cedc99d9aec30f5418beb0e9653
Zen <81418397+somilkhan@users.noreply.github.com>
2026-09-24 14:22:59 +0530

    perf(ui): remove unused Compottie dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -649,7 +649,6 @@ kotlin {
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
-            implementation(libs.compottie)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.serialization.json)
