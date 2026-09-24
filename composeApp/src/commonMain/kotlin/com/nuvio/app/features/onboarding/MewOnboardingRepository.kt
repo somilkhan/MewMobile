8c195e01bfff063aec8fa3d9f162ee8ead032e4f
Zen <81418397+somilkhan@users.noreply.github.com>
2026-09-24 16:27:41 +0530

    fix(onboarding): simplify completion repository

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/onboarding/MewOnboardingRepository.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/onboarding/MewOnboardingRepository.kt
@@ -1,16 +1,8 @@
 package com.nuvio.app.features.onboarding
 
 import com.nuvio.app.features.settings.NuvioEnhancedSettingsRepository
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.distinctUntilChanged
 
 internal object MewOnboardingRepository {
-    val visible: StateFlow<Boolean>
-        get() = NuvioEnhancedSettingsRepository.uiState
-            .map { !it.onboardingCompleted }
-            .distinctUntilChanged() as StateFlow<Boolean>
-
     fun ensureLoaded() {
         NuvioEnhancedSettingsRepository.ensureLoaded()
     }
